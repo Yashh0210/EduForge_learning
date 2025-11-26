90aeb48ec121dd1eea5135f607c84254ea90294e
Yash00222 <yashyadavpw@gmail.com>
2025-11-26 19:24:49 +0530

    a

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 // Connect to the MongoDB database
 const connectDB = async () => {
+  mongoose.connection.on('connected', () => console.log('Database Connected'))
 
-    mongoose.connection.on('connected', () => console.log('Database Connected'))
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
-
+  // Expect a full MongoDB connection string in MONGODB_URI.
+  // Example (no quotes):
+  // MONGODB_URI=mongodb+srv://user:password@cluster0.zv9qfvw.mongodb.net/lms
+  await mongoose.connect(process.env.MONGODB_URI)
 }
 
 export default connectDB
