a0ac206c84628bcd448e4bd84b9cd40a1f16efb2
Yash00222 <yashyadavpw@gmail.com>
2025-11-30 00:28:47 +0530

    navbar updated

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
 
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCoursesListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
-      <img onClick={() => navigate('/')} src={assets.logo} alt="Logo" className="w-28 lg:w-32 cursor-pointer" />
+      <h1 onClick={() => navigate('/')} className="text-2xl lg:text-3xl font-bold text-blue-600 cursor-pointer">EduForge</h1>
       <div className="md:flex hidden items-center gap-5 text-gray-500">
         <div className="flex items-center gap-5">
           {
